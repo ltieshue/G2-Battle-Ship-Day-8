1eaf9cfc4e06eeecb338d1fc71f420b1780b652b
ltieshue <lori.tieshue@gmail.com>
2018-02-02 14:42:47 -0700

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+import java.sql.SQLOutput;
+public static void main(String[1] args)
+public class Main {
+
+
+    // write your code here
+//    {
+//        System.out.println("Select the coordinates of where the ship is.");
+//        String usersInput = sc.nextLine();
+//
+//        System.out.println("Hit!");
+//        System.out.println("Miss!");
+//        System.out.println("These coordinates were previously selected. Please try again!");
+//        String previouslySelected = sc.nextLine();
+//    }
+
+
+}
